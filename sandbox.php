03d136d4238dcaf239f743e741cbd7e6230618fb
Izukanji Sinkolongo <izukanjisinkolongo@gmail.com>
2025-03-13 19:13:06 +0200

    oop in sandbox

--- sandbox.php
+++ sandbox.php
@@ -1,15 +1,50 @@
 <?php 
 
-$score = 50;
+    if(isset($_POST['submit'])){
 
-    if($score > 40){
-        echo 'high score';
-    }else{
-        echo 'low score';
+        setcookie('gender', $_POST['gender'], time() + 86400);
+
+        session_start();
+
+        $_SESSION['name'] = $_POST['name'] ?? 'No Name';
+
+        header('Location: index.php');
     }
 
-    $val = $score > 40 ? 'high score' : 'low score';
-    echo $val;
+    //classes
+    class User {
+        private $email;
+        private $name;
+
+        public function __construct($name, $email){
+            $this->email = $email;
+            $this->name = $name;
+        }
+
+        public function login(){
+            echo $this->name . ' logged in.';
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function getEmail(){
+            return $this->email;
+        }
+
+        public function setName($name){
+            if(is_string($name) && strlen($name) > 1){
+                $this->name = $name;
+                return "name has been updated to $name";
+            }else{
+                return "not a valid name";
+            }
+        }
+    }
+
+    $user = new User('izukanji', 'izukanjisinks@gmail.com');
+    echo $user->setName(5);
 
 ?>
 
@@ -21,6 +56,15 @@ $score = 50;
     <title>Document</title>
 </head>
 <body>
+
+    <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+        <input type="text" name="name">
+        <select name="gender">
+            <option value="male">male</option>
+            <option value="female">female</option>
+        </select>
+        <input type="submit" name="submit" value="submit">
+    </form>
     
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,11 @@
+<?php 
+    session_start();
+    $name = $_SESSION['name'];
+
+    //get cookie
+    $gender = $_COOKIE['gender'] ?? 'Unknown';
+?>
+
 <head>
     <title>Ninja Pizza</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
@@ -20,6 +28,8 @@
         <div class="container">
             <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
+            <li class="grey-text">Hello <?php echo htmlspecialchars($name) ?></li>
+            <li class="grey-text">(<?php echo htmlspecialchars($gender)?>)</li>
                 <li><a href="pages/add.php" class="btn brand z-depth-2">Add a pizza</a></li>
             </ul>
         </div>
